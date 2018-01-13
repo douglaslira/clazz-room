af823145d1510b6fc96b084f1a2df990ab960c1f
Randil Fernando <randil.fernando@outlook.com>
2018-01-14 00:52:01 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const ioInit = require('./app/events/main');
+
+const app = require('http').createServer();
+const io = require('socket.io')(app);
+
+app.listen(3000, () => {
+  console.log('server is listening on http://localhost:3000');
+})
+
+io.set('origins', '*:*');
+ioInit(io);
